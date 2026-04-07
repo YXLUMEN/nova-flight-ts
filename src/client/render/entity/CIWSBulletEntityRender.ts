e117a16ed9d19d7c42c117c7adf2c7f3775c45be
YXLUMEN <lumengrace@outlook.com>
2026-04-07 22:12:45 +0800

    add 中继客户端连接查询;

--- src/client/render/entity/CIWSBulletEntityRender.ts
+++ src/client/render/entity/CIWSBulletEntityRender.ts
@@ -2,15 +2,14 @@ import type {EntityRenderer} from "./EntityRenderer.ts";
 import type {CIWSBulletEntity} from "../../../entity/projectile/CIWSBulletEntity.ts";
 
 export class CIWSBulletEntityRender implements EntityRenderer<CIWSBulletEntity> {
-    public render(entity: CIWSBulletEntity, ctx: CanvasRenderingContext2D, tickDelta: number, offsetX: number = 0, offsetY: number = 0) {
+    public render(entity: CIWSBulletEntity, ctx: CanvasRenderingContext2D, tickDelta: number) {
         const pos = entity.getLerpPos(tickDelta);
-        const x = pos.x + offsetX;
-        const y = pos.y + offsetY;
+        const vel = entity.getVelocityRef;
 
-        const tailX = x - entity.getVelocityRef.x;
-        const tailY = y - entity.getVelocityRef.y;
+        const tailX = pos.x - vel.x;
+        const tailY = pos.y - vel.y;
 
-        const gradient = ctx.createLinearGradient(tailX, tailY, x, y);
+        const gradient = ctx.createLinearGradient(tailX, tailY, pos.x, pos.y);
         gradient.addColorStop(0, "rgba(255, 200, 100, 0)");
         gradient.addColorStop(1, entity.color);
 
@@ -19,7 +18,7 @@ export class CIWSBulletEntityRender implements EntityRenderer<CIWSBulletEntity> 
         ctx.lineWidth = 3;
         ctx.beginPath();
         ctx.moveTo(tailX, tailY);
-        ctx.lineTo(x, y);
+        ctx.lineTo(pos.x, pos.y);
         ctx.stroke();
         ctx.restore();
     }
